0ee05b97b689275b44990d5504e74a9a46775f24
Alexis Stephan <alexis-stephan@hotmail.fr>
2023-05-04 20:21:45 +0200

    add timer
    Signed-off-by: Alexis Stephan <alexis-stephan@hotmail.fr>

--- game.js
+++ game.js
@@ -1,6 +1,10 @@
 const colors = ['red', 'green', 'blue', 'yellow'];
 const gridSize = 5;
 const gameGrid = document.getElementById('gameGrid');
+const timerElement = document.getElementById('timer');
+let elapsedTime = 0;
+let gameInterval;
+let completed = false;
 
 function createGrid() {
     for (let i = 0; i < gridSize * gridSize; i++) {
@@ -19,10 +23,10 @@ function checkAdjacentCells(index) {
     const currentColor = currentCell.style.backgroundColor;
 
     const adjacentCells = [
-        index - gridSize, // Top
-        index + gridSize, // Bottom
-        index % gridSize !== 0 ? index - 1 : -1, // Left
-        (index + 1) % gridSize !== 0 ? index + 1 : -1 // Right
+        index - gridSize,
+        index + gridSize,
+        index % gridSize !== 0 ? index - 1 : -1,
+        (index + 1) % gridSize !== 0 ? index + 1 : -1
     ];
 
     let matches = 0;
@@ -40,6 +44,41 @@ function checkAdjacentCells(index) {
     if (matches > 0) {
         currentCell.style.backgroundColor = '';
     }
+    checkGameCompletion();
+}
+
+function showRulesModal() {
+    const rulesModal = new bootstrap.Modal(document.getElementById('rulesModal'), {
+        keyboard: false
+    });
+    rulesModal.show();
+}
+
+function startTimer() {
+    gameInterval = setInterval(() => {
+        if (!completed) {
+            elapsedTime++;
+            timerElement.innerText = `Temps écoulé : ${elapsedTime}s`;
+        }
+    }, 1000);
+}
+
+function checkGameCompletion() {
+    completed = true;
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        if (gameGrid.children[i].style.backgroundColor !== '') {
+            completed = false;
+            break;
+        }
+    }
+
+    if (completed) {
+        clearInterval(gameInterval);
+        timerElement.style.color = 'green';
+        timerElement.style.fontWeight = 'bold';
+    }
 }
 
 createGrid();
+showRulesModal();
+startTimer();
